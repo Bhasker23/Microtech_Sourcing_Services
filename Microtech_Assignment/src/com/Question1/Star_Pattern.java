951e3a91336a5e927a9b8300ab5b65f1a13a1aa6
Bhasker23 <shrimalibhaskar2398@gmail.com>
2023-06-28 20:13:01 +0530

    createDownPattern method completed

--- Microtech_Assignment/src/com/Question1/Star_Pattern.java
+++ Microtech_Assignment/src/com/Question1/Star_Pattern.java
@@ -1,9 +1,6 @@
 package com.Question1;
 
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.Scanner;
-import java.util.concurrent.CountDownLatch;
 
 public class Star_Pattern {
 
@@ -22,23 +19,54 @@ public class Star_Pattern {
 	    public static void createPattern(String direction, int maxStars) {
 	        if (direction.equalsIgnoreCase("Up")) {
 	            createUpPattern(maxStars);
+	        }else if (direction.equalsIgnoreCase("Down")) {
+	            createDownPattern(maxStars);
 	        }
-//	        else if (direction.equalsIgnoreCase("Down")) {
-//	            createUpPattern(maxStars);
-//	        } else if (direction.equalsIgnoreCase("Left")) {
-//	            createUpPattern(maxStars);
-//	        } else if (direction.equalsIgnoreCase("Right")) {
-//	            createUpPattern(maxStars);
-//	        }
 	    else {
 	            System.out.println("Invalid direction input!");
 	        }
 	    }
 
+		private static void createDownPattern(int maxStars) {
+			int space = 1;
+			int row = (int)Math.ceil(maxStars/2)+1;
+//			System.out.println(row);
+			space = 1;
+			for (int i = 1; i <= 2; i++) {
+				for(int j = 0; j<maxStars-space; j++) {
+	            	System.out.print(" ");
+	            }
+	            System.out.println(" *");
+	        }
+			for (int i = row; i >=1; i--) {
+				int count  = 0;
+		          for(int j = 0; j<space; j++) {
+		            	System.out.print(" ");
+		            }
+		          space = space+2;
+		            int temp = i;
+					   while(temp > 1) {
+						   System.out.print("*"+ " ");
+						   count++;
+						   temp--;
+					   }
+					   int num = 1;
+					   while(num < i) {
+						   System.out.print("*"+" ");
+						   count++;
+						   num++;
+					   }
+					System.out.print("*");
+					System.out.println();
+			}
+			
+			
+		}
+
 		private static void createUpPattern(int maxStars) {
 			int space = 1;
 			double row = Math.ceil(maxStars/2)+1;
-			System.out.println(row);
+//			System.out.println(row);
 			for(int i = 1; i<=row; i++){
 				int count  = 0;
 	          for(int j = 0; j<(maxStars-space); j++) {
@@ -63,11 +91,8 @@ public class Star_Pattern {
 					break;
 				}
 			}
-			
-			
 			space = 1;
 			for (int i = 1; i <= 2; i++) {
-
 				for(int j = 0; j<maxStars-space; j++) {
 	            	System.out.print(" ");
 	            }
